d5f528b6097f57b58c934b31790dd4eca3b10d9b
CenTdemeern1 <timo.herngreen@gmail.com>
2024-04-04 15:17:45 +0200

    Audio stuff progress

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,47 @@
-use std::time::{Duration, Instant};
+#![feature(allocator_api)]
 
-use ctru::{prelude::*, services::gfx::{Flush, Swap, Screen}};
+use std::{io::Read, sync::atomic::{AtomicBool, Ordering}, time::{Duration, Instant}};
+
+use ctru::{
+    linear::LinearAllocator, prelude::*, services::{
+        gfx::{Flush, Screen, Swap},
+        ndsp::{wave::Wave, AudioFormat, InterpolationType, Ndsp, OutputMode}
+    }
+};
 
 const FRAME_DURATION: Duration = Duration::new(0, 41666666); // 1 / 24
+static RUN_AUDIO_THREAD: AtomicBool = AtomicBool::new(true);
 
 fn main() {
     let apt = Apt::new().unwrap();
     let mut hid = Hid::new().unwrap();
     let gfx = Gfx::new().unwrap();
+    let ndsp = Ndsp::new();
     let _console = Console::new(gfx.top_screen.borrow_mut());
+
+    let audio_thread_join_handle = if let Ok(ndsp) = ndsp {
+        Some(std::thread::spawn(|| {
+            while RUN_AUDIO_THREAD.load(Ordering::Relaxed) {
+                // Buffer size should be 24431616, but that's too big I'm going to have to do some buffer manipulation here maybe
+                let mut usagi_flap_pcm: Box<[u8], LinearAllocator> = Box::new_in([0u8; 1048576], LinearAllocator);
+                std::fs::File::open("romfs:/usagi-flap.pcm").unwrap().read_exact(&mut usagi_flap_pcm).unwrap();
+                let mut usagi_flap = Wave::new(
+                    usagi_flap_pcm,
+                    AudioFormat::PCM16Stereo,
+                    true
+                );
+                ndsp.set_output_mode(OutputMode::Stereo);
+                let mut channel = ndsp.channel(0).unwrap();
+                channel.set_format(AudioFormat::PCM16Stereo);
+                channel.set_interpolation(InterpolationType::None);
+                channel.set_sample_rate(48000.);
+                channel.queue_wave(&mut usagi_flap).unwrap();
+            }
+        }))
+    } else {
+        println!("\x1b[33mWarning: NDSP firmware not found.\nContinuing without sound.\x1b[0m");
+        None
+    };
 
     println!("Decoding...");
     let decode_start = Instant::now();
@@ -33,7 +66,9 @@ fn main() {
 
     let mut aris_frame_number = 0;
 
-    println!("\n\nHello, World!");
+    println!("\n\nHello, World! I present to you: Aris");
+    println!("\n(Animation by BlueSechi, song is Usagi Flap)");
+    println!("Homebrew by CenTdemeern1, written in Rust");
     println!("\x1b[29;16HPress Start to exit");
 
     while apt.main_loop() {
@@ -63,5 +98,10 @@ fn main() {
         let frame_end = Instant::now();
         let frame_duration = frame_end.duration_since(frame_start);
         std::thread::sleep(FRAME_DURATION - frame_duration);
+    }
+
+    if Some(audio_thread_join_handle) = audio_thread_join_handle {
+        RUN_AUDIO_THREAD.store(false, Ordering::Relaxed);
+        audio_thread_join_handle.join(); // Make sure the thread can exit because of the co-operative threading model
     }
 }
